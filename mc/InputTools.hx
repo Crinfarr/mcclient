d20a34dfd45bca5cd7521a0c83d8094e8ca8c8fc
Asher Hoeveler <theargon0@gmail.com>
2023-07-20 12:24:42 -0500

    Allow reading from stream

--- mc/InputTools.hx
+++ mc/InputTools.hx
@@ -1,9 +1,34 @@
 package mc;
 
+import haxe.Exception;
 import haxe.io.Input;
 
+using mc.InputTools;
+
 class InputTools {
-    function readVarInt(input:Input) {
-        //TODO streamline to make loading faster
+    public static function readVarInt(input:Input) {
+        final seg = 0x7f;
+        final con = 0x80;
+        var val = 0;
+        var pos = 0;
+        var idx = 0;
+        while (true) {
+            final i = input.readByte();
+            val |= (i & seg) << pos;
+
+            if ((i & con) == 0)
+                break;
+            pos += 7;
+            idx++;
+
+            if (pos >= 32)
+                throw new Exception('VarInt size max reached; value dump:\n${val}');
+        }
+        return val;
+    }
+
+    public static function readStringVarInt(input:Input) {
+        final len = input.readVarInt();
+        return input.read(len).toString();
     }
 }
